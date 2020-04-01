fe4ba78a8aa84c0f1ec6f34cdbc155cf75d7e7a2
Daniil Chervyakov <2yodi@mail.ru>
2020-04-02 02:04:59 +0500

    adding test case for long values in input

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,14 +1,13 @@
-// src/components/Task.stories.js
-
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
-  // Our exports that end in "Data" are not stories.
+  decorators: [withKnobs],
   excludeStories: /.*Data$/,
 };
 
@@ -24,10 +23,18 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-export const Default = () => <Task task={{ ...taskData }} {...actionsData} />;
+export const Default = () => {
+  return <Task task={object('task', { ...taskData })} {...actionsData} />;
+};
 
 export const Pinned = () => (
   <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />
 );
 
 export const Archived = () => <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />;
+
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not! This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
+export const LongTitle = () => (
+  <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
+);
